2a93ed20f8ff3cb6e04e12621edf1b7e393cb010
Luba20 <128219479+Luba20@users.noreply.github.com>
2024-05-11 13:26:51 +0200

    Added a five day weather forecast

--- src/index.js
+++ src/index.js
@@ -57,8 +57,44 @@ function changeCity(event) {
 
   findCity(input.value);
 }
+function showForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHTML = "";
 
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="weather-forecast">
+      <div class="row">
+        <div class="col-2">
+         <div class="weather-forecast-date">
+          ${day}
+         </div>
+          🌤️ 
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">
+              
+          18 
+          </span>
+            <span class="weather-forecast-temperature-min">
+              
+          12
+          </span>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    
+      `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
+}
 let formInput = document.querySelector("#form");
 formInput.addEventListener("submit", changeCity);
 
 findCity("Paris");
+showForecast();
